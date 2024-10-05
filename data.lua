67a999f902cd4b7c8064703b39d34fcc02325d72
OtpSyndraUwU <hexbugnano2003@gmail.com>
2024-10-05 20:01:26 +0200

    de momento me conformaria con que fucionara el icono

--- data.lua
+++ data.lua
@@ -1,2 +1,5 @@
-data.raw["item-with-entity-data"]["locomotive"].icon = "__renfe__/graphics/locomotive/locomotive_full.png"
-data.raw["item-with-entity-data"]["locomotive"].icon_size = 512
+--data.raw["item-with-entity-data"]["locomotive"].icon = "__renfe__/graphics/locomotive/locomotive_full.png"
+--data.raw["item-with-entity-data"]["locomotive"].icon_size = 512
+
+data.raw["locomotive"]["locomotive"].icon = "__renfe__/graphics/locomotive/locomotive_full.png"
+data.raw["locomotive"]["locomotive"].icon_size = 512
